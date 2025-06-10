2b486cb59752429688af6f0235c19aa5eda8779e
Saunok Roy <167688787+sp4m-08@users.noreply.github.com>
2025-06-10 17:57:51 +0530

    ESP32 to Firebase RTDB

--- firebase_new_webinar_2025june.ino
+++ firebase_new_webinar_2025june.ino
@@ -11,10 +11,10 @@
 #define WIFI_PASSWORD "12345678"
 
 // Firebase credentials
-#define API_KEY "AIzaSyBKRC5iZLYW6b2_pBsEFUF-aEoEaqriV20"        // your api key
-#define USER_EMAIL "testuser@gmail.com"    // your email id for auth
-#define USER_PASSWORD "pass1234" // your pass for auth
-#define DATABASE_URL "https://updated-c36b3-default-rtdb.asia-southeast1.firebasedatabase.app/"    // your realtime db url
+#define API_KEY "your_api_key"        // your api key
+#define USER_EMAIL "your_user_email"    // your email id for auth
+#define USER_PASSWORD "your_password" // your pass for auth
+#define DATABASE_URL "your_db_url"    // your realtime db url
 
 // Sensor Pins
 #define DHTPIN 14
